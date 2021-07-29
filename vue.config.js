3dc914c5d40e9629d2d31eafb7ae2e53be69c878
Quang Huy <vqh.quanghuy@gmail.com>
2021-07-29 23:03:23 +0700

    Modify Meta tag

--- vue.config.js
+++ vue.config.js
@@ -6,9 +6,7 @@ module.exports = {
     config
       .plugin('html')
       .tap(args => {
-          args[0].title = `Vu Quang Huy's Porfolio`;
-          args[0].meta = {viewport: 'width=device-width,initial-scale=1.0'};
-          args[0].meta = {property: 'og:image', content: 'https://i.vgy.me/SmZhgD.png'};
+        args[0].title = `Vu Quang Huy's Porfolio`;
       return args;
     })
   }
