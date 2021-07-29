5504f25fb0b401778b8bc13faae438531928b671
Quang Huy <vqh.quanghuy@gmail.com>
2021-07-29 22:55:06 +0700

    Completed Porfolio v1.0

---
+++ src/components/Footer.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-footer
+    dark
+    padless
+  >
+    <v-card
+      class="flex"
+      flat
+      tile
+    >
+      <v-card-title class="justify-center">
+        <strong class="subheading">React me on social networks!<menu type="context"></menu></strong>
+
+        <v-spacer></v-spacer>
+
+        <v-btn
+          v-for="social in socialNetworks"
+          :key="social.icon"
+          :href="social.to"
+          class="mx-4"
+          dark
+          icon
+        >
+          <v-icon size="24px">
+            {{ social.icon }}
+          </v-icon>
+        </v-btn>
+      </v-card-title>
+
+      <v-card-text class="py-2 white--text text-center">
+        {{ new Date().getFullYear() }} — <strong>Vu Quang Huy's porfolio</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+</template>
+<script>
+  export default {
+    data: () => ({
+      socialNetworks: [
+        {icon: 'mdi-facebook', to: 'https://www.facebook.com/vqh.quanghuy/'},
+        {icon: 'mdi-github', to: 'https://github.com/vqh-quanghuy'},
+        {icon: 'mdi-linkedin', to: 'https://www.linkedin.com/in/vqh-quanghuy/'},
+      ],
+    }),
+  }
+</script>
+<style scoped>
+.v-card__title{
+  background-color:  #57C293 !important;
+  border-color:  #57C293 !important;
+}
+.v-card__text{
+  background-color:  #32475B !important;
+  border-color:  #32475B !important;
+}
+</style>
---
+++ src/components/Projects.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="projects-section">
+    <v-container class="pa-5">
+      <h3 data-aos="zoom-in-down"
+        data-aos-duration="500"
+        class="sub-heading font-weight-black text-uppercase text-center">Projects</h3>
+      <v-row class="my-4">
+        <v-col data-aos="fade-up"
+          dense v-for="project in projects" :key="project.title" class="d-flex flex-column" lg="4" md="6" sm="6" cols="12">
+          <v-card
+            class="mx-auto flex d-flex flex-column"
+            max-width="100%"
+            raised>
+            <v-img
+              max-width="100%"
+              max-height="250"
+              min-height="250"
+              :src="project.src"
+            ></v-img>
+            <v-card-title>{{ project.title }}</v-card-title>
+            <v-card-text>
+              <div>{{ project.desc }}</div>
+            </v-card-text>
+            <v-divider class="mx-4"></v-divider>
+
+            <v-card-text>
+              Technologies use:
+              <v-chip-group
+                column
+              >
+                <v-chip
+                  v-for="techno in project.tech"
+                  :key="techno"
+                  text-color="white"
+                  label
+                >
+                  {{ techno }}
+                </v-chip>
+              </v-chip-group>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    projects: [
+      { 
+        title: "Booking Hotel Room Project",
+        desc: "Build a website with hotel booking function...",
+        src: require('@/assets/porfo-projects/booking-project.png'),
+        tech: [
+          "PHP", "Laravel", "Vanilla JS", "Laravel Blade"
+        ]
+      },
+      { 
+        title: "Real Estate Agency",
+        desc: "Build a website handle agency for real estates...",
+        src: require('@/assets/porfo-projects/real-esate-project.png'),
+        tech: [
+          "PHP", "Laravel", "Vanilla JS", "Laravel Blade"
+        ]
+      },
+      { 
+        title: "MBTI Personality Test Project",
+        desc: "Build a new internal function to classify employee personality...",
+        src: require('@/assets/porfo-projects/mbti-project.png'),
+        tech: [
+          "PHP", "Laravel", "Vanilla JS", "Restful API"
+        ]
+      },
+      { 
+        title: "Vhuers Online",
+        desc: "A event site welcome Van Hien University 24 years anniversary",
+        src: require('@/assets/porfo-projects/24yrs-anniversary-project.png'),
+        tech: [
+          "NodeJS", "Express", "VueJS", "Restful API", "HTML Canvas"
+        ]
+      },
+      { 
+        title: "Personal Porfolio",
+        desc: "Personal site show off my education, projects and work experiences",
+        src: require('@/assets/porfo-projects/porfolio-project.png'),
+        tech: [
+          "VueJS", "Vuetify",
+        ]
+      },
+    ]
+  })
+}
+</script>
+<style scoped>
+.sub-heading{
+  font-size: 40px;
+  line-height: 63px;
+  color: #32475B;
+}
+.v-chip{
+  font-weight: bold;
+  background: linear-gradient(90deg, #1CD8D2 0%, #93EDC7 100%) !important;
+  pointer-events: none;
+}
+.v-card{
+  box-shadow: 3px solid #32475B !important;
+}
+@media only screen and (max-width: 600px) {
+  .sub-heading{
+    font-size: 32px;
+    line-height: 42px;
+  }
+}
+</style>
